265b505d828ca26f421ffeb8be8452d3802429d7
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-10-11 17:03:50 +0900

    Application Initialization and Modified Gemfile for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapRamble::Application.config.secret_key_base = '48c3a830cd0a0257d1d5ed26d4d6564847476f2c07a3f14288d0e0ec90cf83dff77897846c81e875b16725ac7f870efa2bd90d851635d9f646e185220aa56084'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MapRamble::Application.config.session_store :cookie_store, key: '_map_ramble_session'

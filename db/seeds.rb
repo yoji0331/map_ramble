3111c01bf7a3e277b0b3d7bb157737f6254d1728
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-10-14 07:59:27 +0900

    seedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# シードを投入するテーブルのリスト(空白区切り)
+table_names = %w(initials places place_images)
+
+# シードデータが存在すれば、シードを投入する
+table_names.each do |table_name|
+  path = Rails.root.join("db/seeds/#{Rails.env}/#{table_name}.rb")
+  if File.exist?(path)
+    puts "Creating #{table_name}..."
+    require path
+  end
+end

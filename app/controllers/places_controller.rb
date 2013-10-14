77778fdf080f7c9bbef4e516ac5b288a09925811
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-10-14 18:58:08 +0900

    placesコントローラーとビューの作成

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
   before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :admin_login_required, except: [:index, :show]
 
   # GET /places
   # GET /places.json
@@ -10,15 +11,57 @@ class PlacesController < ApplicationController
   # GET /places/1
   # GET /places/1.json
   def show
+    if user_signed_in?
+      if current_user.my_map.present?
+        @my_map = current_user.my_map
+      else
+        @my_map = current_user.create_my_map
+      end
+      @way_point = @my_map.registrated(@place)
+      location = current_user.locations.last
+      location_for_json = location
+      if current_user.admin?
+        location = Location.last
+        location_for_json = current_user.locations.last
+      end
+
+      if Checkin::enable_checkin(@place, location)
+        @enable_checkin = true
+      end
+      @checkin = Checkin.where("user_id = ? AND place_id = ?", current_user.id, @place.id).order("updated_at DESC").limit(1)[0]
+      if @checkin.present?
+        @had_been_checkined = true
+      end
+
+      if @enable_checkin.present?
+        @checkin_message = "チェックイン"
+        if @had_been_checkined.present?
+          @checkin_message = "再チェックイン"
+        end
+      end
+    end
+
+    if params[:format].in?(["jpg", "png", "gif"])
+      send_image
+    else
+      render "places/show"
+#    respond_to do |format|
+#      format.html # show.html.erb
+#      format.json { render json: @place }
+    end
   end
 
   # GET /places/new
   def new
     @place = Place.new
+    @categories = Category.all
+    @place.build_image
   end
 
   # GET /places/1/edit
   def edit
+    @categories = Category.all
+    @place.build_image unless @place.image
   end
 
   # POST /places
@@ -70,5 +113,14 @@ class PlacesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
       params.require(:place).permit(:name, :category_id, :lat, :lng, :description)
+    end
+
+    # 画像送信
+    def send_image
+      if @place.image.present?
+        send_data @place.image.data, type: @place.image.content_type, disposition: "inline"
+      else
+        raise NotFount
+      end
     end
 end

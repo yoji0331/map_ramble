090ad48ff5bee64b08b4b65d7dc4c4eb52818f35
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-10-14 18:10:12 +0900

    categoriesコントローラーとビューの作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :admin_login_required, except: [:index, :show]
 
   # GET /categories
   # GET /categories.json

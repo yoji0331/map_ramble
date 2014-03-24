93f075217ca620ade5d16c58e6054362f9baaf7f
Yoji Sawada <smhsc251@yahoo.co.jp>
2014-03-24 18:07:09 +0900

    メールアドレスの変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'kokubo@aomori-u.ac.jp'
+  config.mailer_sender = 'smhsc251@yahoo.co.jp'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

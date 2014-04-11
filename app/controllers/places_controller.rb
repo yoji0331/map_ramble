b07856bc9bdace3c1a02cadc28159e9bf3cfb0d6
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-04-11 18:15:40 +0900

    Placeオブジェクトのimageのuploaded_imageが保存時に許容されるように変更

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -109,7 +109,7 @@ class PlacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
-      params.require(:place).permit(:name, :category_id, :lat, :lng, :description)
+      params.require(:place).permit(:name, :category_id, :lat, :lng, :description, image_attributes: [:uploaded_image])
     end
 
     # 画像送信
